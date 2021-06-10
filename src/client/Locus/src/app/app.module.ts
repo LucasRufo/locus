54ef64a786568e76325ffbaa2e551d3b5fca26b3
Lucas Rufo <rifelu25@outlook.com>
2021-06-09 21:47:11 -0300

    feat: adding balance component

--- src/client/Locus/src/app/app.module.ts
+++ src/client/Locus/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SaldoComponent } from './dashboard/balance/balance.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    DashboardComponent,
+    SaldoComponent,
   ],
   imports: [
     BrowserModule,
